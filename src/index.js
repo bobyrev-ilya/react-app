1454291d303a2914f4bee10fa78f46c63995e62a
bobyrev-ilya <bobyrev-1997@mail.ru>
2021-11-11 20:51:59 +0300

    создал собственный store для хранения state и функций + забайндил ссылки на методы store явным контекстом, чтобы избежать проблем

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, {addNews, subscribe, updateText} from "./redux/state"
+import store from "./redux/state"
 import ReactDOM from "react-dom";
 import App from "./App";
 
 export let rendererEntire = () => {
     ReactDOM.render(
         <App
-            state={state}
-            addNews={addNews}
-            updateText={updateText}
-        />, document.getElementById('root')
+            state={store.getState()}
+            addNews={store.addNews.bind(store)}  //методы жестко связаны с контекстом (this) store
+            updateText={store.updateText.bind(store)}
+        />,
+        document.getElementById('root')
     );
 }
 
-rendererEntire(state);
+rendererEntire(store.getState());
 
-subscribe(rendererEntire);
+store.subscribe(rendererEntire);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,37 +1,42 @@
-let rendererEntire;
-
-let state = {
-    contentPage: {
-        news: [
-            {id: 1, title: "11 oct"},
-            {id: 2, title: "12 oct"}
-        ],
-        comments: [
-            {id: 1, desc: "comm 1"},
-            {id: 2, desc: "comm 2"}
-        ],
-        newText: 'sds'
+let store ={
+    _state : {
+        contentPage: {
+            news: [
+                {id: 1, title: "11 oct"},
+                {id: 2, title: "12 oct"}
+            ],
+            comments: [
+                {id: 1, desc: "comm 1"},
+                {id: 2, desc: "comm 2"}
+            ],
+            newText: 'sds'
+        }
+    },
+    getState(){
+        return this._state;
+    },
+    _callsubscriber() {
+    },
+    addNews(){
+        let contentPage = this._state.contentPage;
+        let currentNews = contentPage.news;
+        currentNews.push({
+                id: currentNews[currentNews.length - 1].id + 1,
+                title: contentPage.newText == "" ? "- No title -" : contentPage.newText
+            }
+        );
+        this.updateText('')
+        this._callsubscriber();
+    },
+    updateText(text){
+        this._state.contentPage.newText = text;
+        this._callsubscriber();
+    },
+    subscribe(observer){
+        this._callsubscriber = observer; // паттерн observer (наблюдатель)
     }
 }
 
-export const addNews = () => {
-    let currentNews = state.contentPage.news;
-    state.contentPage.news.push({
-            id: currentNews[currentNews.length - 1].id + 1,
-            title: state.contentPage.newText == "" ? "- No title -" : state.contentPage.newText
-        }
-    );
-    updateText('')
-    rendererEntire();
-}
+export default store;
 
-export const updateText = (text) => {
-    state.contentPage.newText = text;
-    rendererEntire();
-}
-
-export const subscribe = (observer) =>{
-    rendererEntire = observer; // паттерн observer (наблюдатель)
-}
-
-export default state;
+window.store = store
