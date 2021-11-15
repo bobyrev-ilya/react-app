da912de4d610f3d5f70f6d432eb9dc06ae26f82c
bobyrev-ilya <bobyrev-1997@mail.ru>
2021-11-15 15:59:41 +0300

    Добавил заглушку на получение списка кард по REST

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import axios from "axios";
 
 
 let Cards = (props) => {
 
+    /**
+     * Запрос на сервер для получения списка кард
+     */
     if(props.cards.length == 0){
-        props.setCards([
-            {id: 1, favourited: false, number: "5656262614848", balance: 45522.62},
-            {id: 2, favourited: false, number: "8996666565655", balance: 42.62},
-            {id: 3, favourited: true, number: "5656262614846", balance: 0.0},
-            {id: 4, favourited: false, number: "5656262614844", balance: 17.5}
-        ])
+        axios
+            .get("http://localhost:3000")
+            .then(response => {
+                debugger
+                props.setCards(
+                    [
+                        {id: 1, favourited: false, number: "5656262614848", balance: 45522.62},
+                        {id: 2, favourited: false, number: "8996666565655", balance: 42.62},
+                        {id: 3, favourited: true, number: "5656262614846", balance: 0.0},
+                        {id: 4, favourited: false, number: "5656262614844", balance: 17.5}
+                    ]
+                )
+            });
     }
 
     return <div>
