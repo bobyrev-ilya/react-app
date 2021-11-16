91d0d2b036b8922945d1a332754d99e94f97ad8a
bobyrev-ilya <bobyrev-1997@mail.ru>
2021-11-16 18:14:59 +0300

    В Cards чисто презентационная компонента, CardsContainer.jsx содержит себя промежуточную классовую компонету и возвращает контеерную

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,95 +1,44 @@
 import React from "react";
-import axios from "axios";
-import styles from "./Cards.module.css"
+import styles from "./Cards.module.css";
 
+let Cards = (props) =>{
 
-class Cards extends React.Component {
+    let pagesCount = Math.ceil(props.totalCardsCount / props.pageSize);
 
-    /**
-     * Метод генерации постраничного ответа
-     */
-    generatePagination(currentPage, size) {
-        let test = [
-            {id: 1, favourited: false, number: "5656262614848", balance: 45522.62},
-            {id: 2, favourited: false, number: "8996666565655", balance: 42.62},
-            {id: 3, favourited: false, number: "8996666565655", balance: 42.62},
-            {id: 4, favourited: true, number: "5656262614846", balance: 0.0},
-            {id: 5, favourited: true, number: "5656262614846", balance: 0.0},
-            {id: 6, favourited: false, number: "5656262614844", balance: 17.5},
-            {id: 7, favourited: false, number: "5656262614844", balance: 17.5}
-        ];
+    let pages = [];
 
-        return {
-            data: test.slice(size * (currentPage - 1), Math.min(test.length, size * (currentPage - 1) + size)),
-            totalCount: test.length
-        }
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
 
-
-    componentDidMount() {
-        axios
-            .get("http://localhost:3000")
-            .then(() => {
-                let resp = this.generatePagination(this.props.currentPage, this.props.pageSize);
-                this.props.setCards(resp.data);
-                this.props.setCardsCount(resp.totalCount);
-            });
-    }
-
-    onPageChanged = (pageNumber) => {
-
-        this.props.setCurrentPage(pageNumber);
-
-        axios
-            .get("http://localhost:3000")
-            .then(() => {
-                this.props.setCards(
-                    this.generatePagination(pageNumber, this.props.pageSize).data
-                );
-            });
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalCardsCount / this.props.pageSize);
-
-        let pages = [];
-
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        return <div>
-            <div>
-                {pages.map(p =>
-                    <span className={this.props.currentPage === p && styles.selectedPage}
-                          onClick={() => {
-                              this.onPageChanged(p)
-                          }}>{p}</span>)
-                }
-            </div>
-            {
-                this.props.cards.map(c => <div key={c.id}>
-                    <div>
+    return <div>
+        <div>
+            {pages.map(p =>
+                <span className={props.currentPage === p && styles.selectedPage}
+                      onClick={() => {props.onPageChanged(p)}}>{p}</span>)
+            }
+        </div>
+        {
+            props.cards.map(c => <div key={c.id}>
+                <div>
                     <span>
                         <span>
                             {c.favourited ?
-                                <button onClick={() => this.props.unfavourite(c.id)}>Unfavoured</button> :
-                                <button onClick={() => this.props.favourite(c.id)}>Favoured</button>
+                                <button onClick={() => props.unfavourite(c.id)}>Unfavoured</button> :
+                                <button onClick={() => props.favourite(c.id)}>Favoured</button>
                             }
                         </span>
                         <span>
                             {c.number}
                         </span>
                     </span>
-                    </div>
-                    <div>
-                        {`Баланс: ${c.balance}`}
-                    </div>
-                </div>)
-            }
-        </div>
-    }
+                </div>
+                <div>
+                    {`Баланс: ${c.balance}`}
+                </div>
+            </div>)
+        }
+    </div>
 }
 
-export default Cards
+export default Cards;
--- src/components/Cards/CardsContainer.jsx
+++ src/components/Cards/CardsContainer.jsx
@@ -7,7 +7,68 @@ import {
     setTotalCardsCountAC
 } from "../../redux/cards-reducer";
 import {connect} from "react-redux";
-import CardsC from "./Cards";
+import axios from "axios";
+import Cards from "./Cards";
+
+/**
+ * Классовая компонета для вызова API
+ */
+class CardsContainer extends React.Component {
+
+    /**
+     * Метод генерации постраничного ответа
+     */
+    generatePagination(currentPage, size) {
+        let test = [
+            {id: 1, favourited: false, number: "5656262614848", balance: 45522.62},
+            {id: 2, favourited: false, number: "8996666565655", balance: 42.62},
+            {id: 3, favourited: false, number: "8996666565655", balance: 42.62},
+            {id: 4, favourited: true, number: "5656262614846", balance: 0.0},
+            {id: 5, favourited: true, number: "5656262614846", balance: 0.0},
+            {id: 6, favourited: false, number: "5656262614844", balance: 17.5},
+            {id: 7, favourited: false, number: "5656262614844", balance: 17.5}
+        ];
+
+        return {
+            data: test.slice(size * (currentPage - 1), Math.min(test.length, size * (currentPage - 1) + size)),
+            totalCount: test.length
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get("http://localhost:3000")
+            .then(() => {
+                let resp = this.generatePagination(this.props.currentPage, this.props.pageSize);
+                this.props.setCards(resp.data);
+                this.props.setCardsCount(resp.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+
+        this.props.setCurrentPage(pageNumber);
+
+        axios
+            .get("http://localhost:3000")
+            .then(() => {
+                this.props.setCards(
+                    this.generatePagination(pageNumber, this.props.pageSize).data
+                );
+            });
+    }
+
+    render() {
+        return <Cards totalCardsCount={this.props.totalCardsCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      cards={this.props.cards}
+                      onPageChanged={this.onPageChanged}
+                      favourite={this.props.favourite}
+                      unfavourite={this.props.unfavourite}
+        />;
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -38,4 +99,5 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CardsC)
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardsContainer)
